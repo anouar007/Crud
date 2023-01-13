0c0b11e7c230f534ac7647c15828aeac73b10465
anouar007 <38794684+anouar007@users.noreply.github.com>
2023-01-13 21:49:20 +0100

    Create clientsController.php

---
+++ clientsController.php
@@ -0,0 +1,19 @@
+<?php
+include "init.php";
+
+if (isset($_POST["request"])) {
+    switch ($_POST["request"]) {
+        case 'get':
+            
+            break;
+        case 'add':
+
+            break;
+        case 'update':
+            
+            break;
+        case 'delete':
+            
+            break;
+    }
+}
