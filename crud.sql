1c324e4c04f053c309daed4c211652a95d4316fb
ayoub chamchi <47029182+achamchi01@users.noreply.github.com>
2023-01-14 12:08:47 +0000

    update db

--- crud.sql
+++ crud.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jan 14, 2023 at 11:47 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.19
+-- Host: 127.0.0.1
+-- Generation Time: Jan 14, 2023 at 01:08 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,30 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `clients` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+  `email` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `clients`
---
-
-INSERT INTO `clients` (`id`, `name`, `email`) VALUES
-(1, 'client 1', 'email 1'),
-(2, 'client 2', 'email 2'),
-(3, 'client 3', 'email 3'),
-(4, 'client 4', 'email 4'),
-(5, 'client 5', 'email 5'),
-(12, 'client 6', 'email 6'),
-(13, 'client 7', 'email 7'),
-(14, 'client 8', 'email 8'),
-(15, 'client 9', 'email 9'),
-(16, 'client 10', 'email 10'),
-(17, 'client 11', 'email 11'),
-(18, 'client 12', 'email 12'),
-(19, 'client 13', 'email 13'),
-(20, 'client 14', 'email 14'),
-(21, 'client 15', 'email 15'),
-(22, 'client 16', 'email 16');
 
 --
 -- Indexes for dumped tables
@@ -73,7 +52,7 @@ ALTER TABLE `clients`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
