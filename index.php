37e3628646908a1edef7d826b9960d2eeda14028
anouar007 <38794684+anouar007@users.noreply.github.com>
2023-01-14 09:17:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,15 +14,15 @@ include "init.php";
 </head>
 <body>
 <div class="container">
-    <div class="row mb-2 mt-5">
-        <div class="col col-12">
-            <h1>CRUD Project</h1> 
+        <div class="row mb-2 mt-4">
+            <div class="col col-12">
+                <h1>CRUD Project</h1>
+            </div>
+            <div class="col col-12">
+                <button class="btn btn-primary float-end btn-sm" id="newClient" data-bs-toggle="modal" data-bs-target="#newClientModal">Add Client</button>
+            </div>
         </div>
-        <div class="col col-12">
-            <button class="btn btn-primary float-end btn-sm" id="newClient">Add Client</button>
-        </div>
-    </div>
-    <div class="row pt-2" style="box-shadow: 1px 1px 3px 0px #c3c3c3;">
+    <div class="row pt-2 pb-2" style="box-shadow: 1px 1px 3px 0px #c3c3c3;">
         <table class="table w-100 table-dark table-striped table-hover " id="clientsTable">
             <thead>
                 <tr>
@@ -35,6 +35,36 @@ include "init.php";
         </table>
     </div>
 </div>
+
+<!-- New Client -->
+<div class="modal fade" id="newClientModal" tabindex="-1" aria-labelledby="newClientModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="enewClientModalLabel">Create new Client</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Client Name</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="nameHelp">
+                            <div id="nameHelp" class="form-text"></div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email1" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email1" aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text"></div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
 <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
